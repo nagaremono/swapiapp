1660726fde2ee268bef464d81ca7058039b8886d
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-11-13 09:26:55 +0800

    set contrasting background color

--- src/components/MainWrapper.tsx
+++ src/components/MainWrapper.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 
 export const MainWrapper: React.FC = ({ children }) => {
   return (
-    <Box minHeight="100vh" bg="main.primary" py={4}>
-      <Container boxShadow="lg main.white" py={4} color="main.white">
+    <Box minHeight="100vh" bg="main.secondary" py={4}>
+      <Container
+        bg="main.primary"
+        boxShadow="lg main.white"
+        py={4}
+        color="main.white"
+      >
         {children}
       </Container>
     </Box>
